9499d265f244cca3944430d5bf340ec16b3989ed
Nat Welch <nat@natwelch.com>
2013-03-09 00:13:34 -0800

    wheee

--- 2013/q4/robot.rb
+++ 2013/q4/robot.rb
@@ -12,6 +12,13 @@ def main lines
     p.x, p.y = line.split(',').map {|a| a.to_f }
     points.push p
   end
+
+  d = 0
+  points[0...-1].each_index do |i|
+    d += Math.sqrt((points[i+1].x - points[i].x)**2 + (points[i+1].y - points[i].y)**2)
+  end
+
+  puts d.round(3)
 end
 
 main(ARGF.readlines.map {|i| i.strip })
